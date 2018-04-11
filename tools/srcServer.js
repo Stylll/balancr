e2804b9d9a592043cb5f279d1b5435c8a978c107
Styll <stephen.aribaba@yahoo.com>
2018-04-11 22:16:53 +0100

    Initial files, folders and dependencies installed

---
+++ tools/srcServer.js
@@ -0,0 +1,29 @@
+import express from 'express';
+import path from 'path';
+import open from 'open';
+import logger from 'morgan';
+import bodyParser from 'body-parser';
+
+/* eslint-disable no-console */
+
+const port = 3000;
+const app = express();
+
+// Middleware
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.get('/*', (req, res) => {
+  res.status(200).send({ message: 'Welcome to balancr.' });
+});
+
+app.listen(port, (err) => {
+  if (err) {
+    console.log(err);
+  } else {
+    open(`http://localhost:${port}`);
+  }
+});
+
+export default app;
